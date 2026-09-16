96094d3a6b57b4d27526f00dc38277e7b961e5dc
krshksss <kirpro799@gmail.com>
2026-09-16 17:33:26 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "life-v8";
+const CACHE = "life-v11";
 const ASSETS = ["./", "./index.html", "./css/style.css", "./js/app.js", "./js/fx.js", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", (e) => {
